783d963f715e3b4199b69d71152151c078f3eb69
egesua <egesu06@gmail.com>
2024-02-27 22:44:23 +0300

    user controller and service layer completed.

--- src/main/java/com/egesua/connectHub/controller/UserController.java
+++ src/main/java/com/egesua/connectHub/controller/UserController.java
@@ -1,54 +1,44 @@
 package com.egesua.connectHub.controller;
 
 import com.egesua.connectHub.entity.User;
-import com.egesua.connectHub.repository.UserRepository;
+import com.egesua.connectHub.services.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    private UserRepository userRepository;
+    private UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return userService.getAllUsers();
     }
 
     @PostMapping
     public User createUser(@RequestBody User newUser) {
-        return userRepository.save(new User());
+        return userService.saveOneUser(new User());
     }
 
     @GetMapping("/{userId}")
     public User getOneUser(@PathVariable Long userId) {
-        //custom exception ekle...
-        return userRepository.findById(userId).orElse(null);
+        //custom exception...
+        return userService.getOneUser(userId);
     }
 
     @PutMapping("/{userId}")
     public User updateOneUser(@PathVariable Long userId, @RequestBody User newUser) {
-        Optional<User> user = userRepository.findById(userId);
-        if (user.isPresent()) {
-            User foundUser = user.get();
-            foundUser.setUsername(new User().getUsername());
-            foundUser.setPassword(newUser.getPassword());
-            userRepository.save(foundUser);
-            return foundUser;
-        } else {
-            return null;
+        return userService.updateOneUser(userId, newUser);
         }
-    }
 
     @DeleteMapping("/{userId}")
     public void deleteOneUser(@PathVariable Long userId){
-        userRepository.deleteById(userId);
+        userService.deleteById(userId);
     }
 
 }
---
+++ src/main/java/com/egesua/connectHub/services/UserService.java
@@ -0,0 +1,45 @@
+package com.egesua.connectHub.services;
+
+import com.egesua.connectHub.entity.User;
+import com.egesua.connectHub.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveOneUser(User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    public User getOneUser(Long userId) {
+        return userRepository.findById(userId).orElse(null);
+    }
+
+    public User updateOneUser(Long userId, User newUser) {
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            User foundUser = user.get();
+            foundUser.setUsername(newUser.getUsername());
+            foundUser.setPassword(newUser.getPassword());
+            userRepository.save(foundUser);
+            return foundUser;
+        } else {
+            return null;
+    }}
+    public void deleteById(Long userId) {
+        userRepository.deleteById(userId);
+    }
+
+}
