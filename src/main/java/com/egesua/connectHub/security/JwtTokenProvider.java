dcc2742c9f85947e72169bcac32d8dbf45124d45
egesua <egesu06@gmail.com>
2024-03-04 21:41:34 +0300

    jwt token creator created.

---
+++ src/main/java/com/egesua/connectHub/security/JwtTokenProvider.java
@@ -0,0 +1,27 @@
+package com.egesua.connectHub.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${connecthub.app.secret}")
+    private String APP_SECRET;
+    @Value("${connecthub.expires.in}")
+    private Long EXPIRES_IN;
+
+    public String generateJwtToken(Authentication auth) {
+        JwtUserDetails userDetails = (JwtUserDetails) auth.getPrincipal();
+        Date expireDate = new Date(new Date().getTime() + EXPIRES_IN);
+        return Jwts.builder().setSubject(Long.toString(userDetails.getId()))
+                .setIssuedAt(new Date()).setExpiration(expireDate)
+                .signWith(SignatureAlgorithm.ES256, APP_SECRET).compact();
+    }
+
+}
